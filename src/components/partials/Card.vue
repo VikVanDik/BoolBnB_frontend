94acefdfdc9bd6bde40a9277729e20825f6b43cb
EmanueleDiLeo <dileoemanuele11@gmail.com>
2024-01-17 21:01:53 +0100

    [ADD]: home and search apartment

---
+++ src/components/partials/Card.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "Card",
+  props: {
+    apartment:Object,
+  },
+
+}
+</script>
+
+
+<template>
+  <div class="card h-100">
+    <div class=" card-img w-100 overflow-hidden" style="height: 220px;">
+        <img :src="apartment.img" class="img-fluid w-100" :alt="apartment.title">
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">{{apartment.title}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ apartment.address }}.</h6>
+        <p v-html="apartment.description"></p>
+        <a href="#" class="btn btn-success">Info</a>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,70 @@
 <script>
+import axios from 'axios';
+import {store} from '../data/store';
+import Card from '../components/partials/Card.vue';
 export default{
   name: 'Home',
+  components:{
+    Card,
+  },
+  props:{
+
+  },
+  data(){
+    return {
+      store,
+    }
+  },
+  methods:{
+    getApi(endpoint){
+      // this.isLoader = false,
+      axios.get(endpoint)
+        .then(results => {
+          // this.isLoader = true;
+          store.apartments = results.data;
+        })
+    }
+  },
+  mounted(){
+    this.getApi(store.apiUrl + 'apartments');
+  }
+
 }
 
 </script>
 
 <template>
 
-<div class="container">
+<div class="container pt-80">
   <h1 class="text-center">Home</h1>
+
+  <div class="container pt-80">
+    <form class="form-inline my-2 my-lg-0">
+
+      <input 
+        class="form-control mr-sm-2" 
+        type="search" 
+        placeholder="Search"
+        v-model="store.toSearch"
+        required>
+
+      <router-link  
+        class="btn btn-outline-success my-2 my-sm-0" 
+        :to="{name: 'search'}">
+          cerca</router-link>
+    </form>
+  </div>
+
+  <div class="mt-5">
+    <h1 class="text-center">Appartamenti Sponsorizzati</h1>
+
+    <div class="row">
+      <div class="col-4 py-3" v-for="apartment in store.apartments" :key="apartment.id">
+        <Card :apartment="apartment"/>
+      </div>
+    </div>
+    
+  </div>
 </div>
 
 </template>
