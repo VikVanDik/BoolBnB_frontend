027ff4b1f06e96ee00d38f400003ce0c03aa4d17
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-01-20 14:57:25 +0100

    [dd]:component form email and send email in post form

---
+++ src/components/FormMessage.vue
@@ -0,0 +1,134 @@
+<script>
+import axios from "axios";
+import {store} from '../data/store';
+
+
+export default {
+  name:'ContactsForm',
+  props:{
+    apartment_id: Number,
+  },
+
+  data() {
+    return{
+      store,
+      sender_email:'',
+      text:'',
+      success:false,
+      errors:{
+        sender_email:[],
+        text:[]
+      },
+   }
+
+  },
+  methods: {
+    sendForm(){
+      const data = {
+        sender_email:this.sender_email,
+        text:this.text,
+        apartment_id:this.apartment_id
+      }
+      
+      axios.post(store.apiUrl + 'send-email' ,data)
+        .then(response => {
+          this.success = response.data.success;
+          console.log(response);
+          if(!this.success){
+            this.errors = response.data.errors
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  }
+}
+</script>
+
+
+<template>
+  
+ 
+<form   class="modal" id="invisible-form" tabindex="-1" @submit.prevent="sendForm()">
+  <div  v-if="!success" class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-danger">Invia il messaggio</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+    <div class="modal-body">
+      <div>
+        <label for="email" ><strong class="text-danger">Email</strong></label>
+        <input v-model="sender_email" type="text" id="email" name="email" >
+        <p class="error-msg" v-for="(error,index) in errors.sender_email" :key="index.error">{{ error }}</p>
+      </div>
+      <div>
+        <label for="message" ><strong class="text-danger">Messaggio</strong> </label>
+        <textarea v-model="text" id="message" name="message" ></textarea>
+        <p class="error-msg" v-for="(error,index) in errors.text" :key="index.error">{{ error }}</p>
+      </div>
+      <button type="submit">Submit</button>                                  
+      </div>                                    
+    </div>
+  </div>
+  <div class="sended modal-dialog modal-dialog-centered" v-else>
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-danger">Messaggio inviato con successo!</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>                                
+    </div>
+  </div>
+</form>
+
+
+<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#invisible-form">Invia Messaggio</button>
+</template>
+
+
+<style lang="scss" scoped>
+
+.sended {
+  margin-top:40px ;
+  h1 {
+    color: green;
+  }
+}
+
+form{
+  min-width: 500px;
+  border: 1px solid black;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  input {
+    margin: 10px 0;
+    padding-left: 10px;
+    display: block;
+    width: 100%;
+    height: 30px;
+    border-radius: 10px;
+  }
+  textarea {
+    padding: 10px;
+    display: block;
+    height: 200px;
+    width: 100%;
+    margin: 10px 0;
+  }
+  button {
+    padding:5px 10px ;
+    &:hover {
+      scale: 1.1;
+      cursor: pointer;
+    }
+  }
+  .error-msg {
+    color: red;
+    margin: 10px 0;
+    font-weight: 700;
+  }
+}
+
+</style>
--- src/pages/ApartmentDetails.vue
+++ src/pages/ApartmentDetails.vue
@@ -1,8 +1,12 @@
 <script>
   import axios  from 'axios';
   import { store } from '../data/store';
+  import FormMessage from '../components/FormMessage.vue';
   export default {
     name:'ApartmentDetails',
+    components:{
+      FormMessage
+    },
 
     data(){
       return {
@@ -44,6 +48,7 @@
     <div class="d-flex justify-content-center">
       <h2>Title Apartment: {{ apartment.title }} </h2>
     </div>
+    <FormMessage :apartment_id = "apartment.id"/>
 
 </template>
 
