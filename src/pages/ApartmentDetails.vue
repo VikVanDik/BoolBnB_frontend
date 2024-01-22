6b63fe053531c89a5d70538c4b38f86ea58445a0
Melozzo <carmecamma1995@gmail.com>
2024-01-22 20:06:10 +0100

    [fix]: style

--- src/pages/ApartmentDetails.vue
+++ src/pages/ApartmentDetails.vue
@@ -26,13 +26,8 @@
         this.apartment = response.data.apartment;
         
         })},
-     
-
     },
     computed: {
-    
-
-     
     },
 
     mounted(){
@@ -44,25 +39,35 @@
 
 
 <template>
-    <div class="container-fluid">
-      <div>
-        <h2>Title Apartment: {{ apartment.title }} </h2>
+    <div class="container my-5 py-5">
+      <h2 class="text-center">Appartamento: {{ apartment.title }} </h2>
+      <div class="d-flex justify-content-center">
+        <div class="me-5 w-50">
+          <img :src="apartment.img" :alt="apartment.title" class="w-100">
+        </div>
+        <div class="d-flex flex-column justify-content-between ">
+          
+          <!-- info appartamento -->
+          <div>
+            <h3>Info appartamento</h3>
+            <p><strong>Numero di stanze: </strong>{{apartment.room_number }}</p>
+            <p><strong>Numero di letti: </strong>{{apartment.bed_number }}</p>
+            <p><strong>Numero di bagni: </strong>{{apartment.bathroom_number }}</p>
+            <p><strong>Mq: </strong>{{apartment.sq_metres }}</p>
+            <div v-if = "apartment.description">
+              <h5>Descrizione:</h5>
+              <p  v-html="apartment.description"></p>
+            </div>
+            <h4 v-else>Nessuna descrizione</h4>
+          </div>
+
+          <!-- bottone -->
+          <div class="button">
+            <FormMessage :apartment_id = "apartment.id"/>
+          </div>
+        </div>
+        <!-- descrizione -->
       </div>
-      <div class="card-img w-100 overflow-hidden" >
-        <img :src="apartment.img" :alt="apartment.title">
-      </div>
-      <div>
-        <p><strong>Numero di stanze: </strong>{{apartment.room_number }}</p>
-        <p><strong>Numero di letti: </strong>{{apartment.bed_number }}</p>
-        <p><strong>Numero di bagni: </strong>{{apartment.bathroom_number }}</p>
-        <p><strong>Mq: </strong>{{apartment.sq_metres }}</p>
-      </div>
-      <div v-if = "apartment.description">
-        <h5>Descrizione:</h5>
-        <p  v-html="apartment.description"></p>
-      </div>
-      <h4 v-else>Nessuna descrizione</h4>
-      <FormMessage :apartment_id = "apartment.id"/>
     </div>
 
 </template>
