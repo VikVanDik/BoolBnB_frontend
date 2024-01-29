93385cf53b341453b1bbda4f90856ffe882df5e9
Melozzo <carmecamma1995@gmail.com>
2024-01-29 18:17:00 +0100

    [add]: style updating

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -10,14 +10,42 @@ export default{
   
   <!-- logo -->
   <router-link class="fs-5 router mx-2" :to="{name: 'home'}">
-    <div class="logo d-flex justify-content-center align-items-center">
+    <div class="logo d-flex justify-center align-items-center">
       <img src="@/img/logo.png" alt="logo">
       <span class="ms-1 text-white">Boolbnb</span>
     </div>
   </router-link>
   
+  <div class="d-md-none">
+    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+      <i class="fa-solid fa-bars text-white"></i>
+    </button>
+
+    <div class="offcanvas offcanvas-end w-50" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" style="background-color: #4b82c0;">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title text-white" id="offcanvasRightLabel">BoolMenu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+
+      <div class="offcanvas-body">
+        <ul>
+          <li>
+            <router-link class="fs-3 text-white" :to="{name: 'home'}">Home</router-link>
+          </li>
+          <li>
+            <router-link class="fs-3 text-white" :to="{name: 'infoBoolbnb'}">Chi siamo</router-link>
+          </li>
+          <li>
+            <router-link class="fs-3 text-white" :to="{name: 'contacts'}">Contatti</router-link>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+
   <!-- menu -->
-  <div class="d-flex justify-content-center align-items-center">
+  <div class="d-flex justify-content-center align-items-center d-none d-md-block">
     <ul class="d-flex">
       <li>
         <router-link class="fs-5 text-white mx-2" :to="{name: 'home'}">Home</router-link>
@@ -32,8 +60,8 @@ export default{
   </div>
   
   <!-- section to register profile and login -->
-  <div class="d-flex justify-content-end align-items-center profile">
-    <ul class="d-flex justify-content-between align-items-center">
+  <div class="profile d-none d-md-block">
+    <ul class="d-flex justify-content-end h-100 align-items-center">
       <li class="text-center pe-2">
         <a href="http://127.0.0.1:8000/login" target="_blank">
           <i class="fa-solid fa-right-to-bracket fa-lg text-white"></i>
@@ -48,10 +76,7 @@ export default{
       </li>
     </ul>
   </div>
-  
 </header>
-
-
 </template>
 
 <style lang="scss" scoped>
