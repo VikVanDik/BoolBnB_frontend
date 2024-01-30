07cc0efde0b53b8bab641f2116720d228b0b80ef
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-01-30 14:18:28 +0100

    [add]:elements on show details page

--- src/pages/ApartmentDetails.vue
+++ src/pages/ApartmentDetails.vue
@@ -28,7 +28,7 @@
         this.apartment = response.data.apartment;
         this.user_name = response.data.apartment.user.name;
         this.user_surname = response.data.apartment.user.surname;
-        
+
         })},
     },
     computed: {
@@ -37,6 +37,7 @@
     mounted(){
       this.slug = this.$route.params.slug;
       this.getSingleProject(this.slug);
+      
     }
   }
   </script>
@@ -44,41 +45,157 @@
 
 <template>
     <div class="container my-5 py-5">
-      <h2 class="text-center">Appartamento: {{ apartment.title }} </h2>
-      <div class="d-flex justify-content-center">
-        <div class="me-5 w-50">
+      <h2 class="mb-4">Appartamento: {{ apartment.title }} </h2>
+      <div class="d-flex flex-column">
+        <div class="me-5 w-75">
           <img :src="apartment.img" :alt="apartment.title" class="w-100">
         </div>
-        <div class="d-flex flex-column justify-content-between ">
-          
+
+        <div class="row">
           <!-- info appartamento -->
-          <div>
-            <h3>Info appartamento di : {{  this.user_name + ' ' + this.user_surname }}</h3>
-            <div>
-              <span class="badge text-bg-info me-2" v-for="service in apartment.services" :key="service.id">{{ service.name }}</span>
+          <!-- colonna di sinistra -->
+          <div class="col-12 col-md-6">
+            
+            <!-- indirizzo e info -->
+            <div class="address">
+              <h3 class="mt-4">Indirizzo : {{ apartment.address }} </h3>
+              <span> {{apartment.room_number }} stanze &#9675;</span>
+              <span> {{apartment.bed_number }} camere da letto &#9675;</span>
+              <span> {{apartment.bathroom_number }} bagni &#9675;</span>
+              <span> {{apartment.sq_metres }} metri quadri</span>
             </div>
-            <p><strong>Numero di stanze: </strong>{{apartment.room_number }}</p>
-            <p><strong>Numero di letti: </strong>{{apartment.bed_number }}</p>
-            <p><strong>Numero di bagni: </strong>{{apartment.bathroom_number }}</p>
-            <p><strong>Mq: </strong>{{apartment.sq_metres }}</p>
-            <div v-if = "apartment.description">
-              <h5>Descrizione:</h5>
-              <p  v-html="apartment.description"></p>
+
+            <!-- info host -->
+            <div class="host mt-4">
+              <h3>Nome dell' host : {{  this.user_name + ' ' + this.user_surname }}</h3>
+              <p>Superhost 9 anni di esperienza come host</p>
             </div>
-            <h4 v-else>Nessuna descrizione</h4>
+
+            <!-- ranking host  -->
+            <div class="ranking-host">
+              <div class="row align-items-center">
+                <div class="col-auto">
+                  <i class="fa-solid fa-medal"></i>
+                </div>
+                <div class="col">
+                  <h3>{{  this.user_name }}  è un Superhost.</h3>
+                  <p>I Superhost sono host con esperienza e valutazioni molto alte.</p>
+                </div>
+              </div>
+
+
+              <div class="row align-items-center">
+                <div class="col-auto">
+                  <i class="fa-solid fa-key"></i>
+                </div>
+                <div class="col">
+                  <h3>Ottima esperienza di check-in</h3>
+                  <p>Il 100% degli ospiti ha valutato con 5 stelle la procedura di check-in.</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- descrizione -->
+            <div class="description">
+              <div v-if = "apartment.description">
+                <h3>Descrizione</h3>
+                <p  v-html="apartment.description"></p>
+              </div>
+              <h4 v-else>Nessuna descrizione</h4>
+            </div>
+
+            <!-- dove dormirai -->
+            <h3>Dove dormirai</h3>
+            <div class="details-house">
+              <div class="row">
+                <div class="col-12 col-md-4">
+                  <div class="card">
+                    <div class="card-body">
+                      <i class="fa-solid fa-door-closed me-1"></i>
+                      <h6 class="card-title my-3">Numero di stanze nell' appartamento</h6>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">{{apartment.room_number }}</h6>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-12 my-3 col-md-4 my-md-0">
+                  <div class="card">
+                    <div class="card-body">
+                      <i class="fa-solid fa-bed me-1"></i>
+                      <h6 class="card-title my-3">Camere da letto nell' appartamento</h6>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">{{apartment.bed_number }}</h6>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 col-md-4">
+                  <div class="card">
+                    <div class="card-body">
+                      <i class="fa-solid fa-toilet"></i>
+                      <h6 class="card-title my-3">Numero di bagni nell' appartamento</h6>
+                      <h6 class="card-subtitle mb-2 text-body-secondary"> {{apartment.bathroom_number }}</h6>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <!-- cosa troverai -->
+            <h3 class="mt-4">Cosa troverai</h3>
+            <div class="services row">
+                <div class="d-flex align-items-center col-6 mb-3" v-for="service in apartment.services" :key="service.id">
+                  <img :src="service.icon" class="service-icon" alt="">     
+                  <span>{{service.name}}</span>
+                </div>
+              
+            </div>
+
+            <h3 class="mt-4">Dove ti troverai</h3>
+
+          </div>
+          <!-- fine colonna sinistra  -->
+
+          <div class="col-auto">
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Chiedi informazioni all' host</h5>
+                <p class="card-text">Di solito l' host risponde entro 3 hr</p>
+                <div class="button">
+                  <FormMessage :apartment_id = "apartment.id" :user_name="this.user_name"  :user_surname="this.user_surname"/>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <!-- bottone -->
-          <div class="button">
-            <FormMessage :apartment_id = "apartment.id" :user_name="this.user_name"  :user_surname="this.user_surname"/>
-          </div>
         </div>
-        <!-- descrizione -->
+        
+          <!-- bottone -->
       </div>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
+i {
+  color:rgb(75, 130, 192);
+  font-size: 25px;
+}
+  .address{
+    span{
+      font-size: .9rem;
+      padding-right: 10px;
+    }
+  }
+
+  .host, .ranking-host , .description, .services{
+    border-bottom: 1px solid rgba(128, 128, 128, 0.244);
+    margin-bottom: 20px;
+  }
+  .service-icon {
+    width: 50px;
+  }
+
+  
   
 </style>
